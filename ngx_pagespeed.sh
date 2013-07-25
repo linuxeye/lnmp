c751ad5961ddfe7ba4f2532fe12d740312f3c21b
lj2007331 <lj2007331@163.com>
2013-07-25 16:02:48 +0800

    add ngx_pagespeed

--- ngx_pagespeed.sh
+++ ngx_pagespeed.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+echo "#######################################################################"
+echo "#                    LNMP for CentOS/RadHat 5/6                       #"
+echo "# For more information please visit http://blog.linuxeye.com/318.html #"
+echo "#######################################################################"
+echo ''
+read -p "Do you want to install ngx_PageSpeed? (y/n)" nps_yn
+if [ $nps_yn == 'y' ];then
 cd /root/lnmp/source
-wget https://github.com/pagespeed/ngx_pagespeed/archive/release-1.6.29.3-beta.zip
+wget --no-check-certificate https://github.com/pagespeed/ngx_pagespeed/archive/release-1.6.29.3-beta.zip
 unzip -q release-1.6.29.3-beta
 wget https://dl.google.com/dl/page-speed/psol/1.6.29.3.tar.gz
 tar xzf 1.6.29.3.tar.gz -C ngx_pagespeed-release-1.6.29.3-beta
@@ -16,5 +23,8 @@ kill -USR2 `cat /usr/local/nginx/logs/nginx.pid`
 kill -QUIT `cat /usr/local/nginx/logs/nginx.pid.oldbin`
 mkdir /var/ngx_pagespeed_cache
 chown www.www /var/ngx_pagespeed_cache
-cp /root/lnmp/vhost.sh /root/lnmp/vhost_no_ngx_pagespeed.sh
-sed -i 's@root $vhostdir;@root $vhostdir;\n\tpagespeed on;\n\tpagespeed FileCachePath /var/ngx_pagespeed_cache;\n\tlocation ~ "\\.pagespeed\\.([a-z]\\.)?[a-z]{2}\\.[^.]{10}\\.[^.]+" { add_header "" ""; }\n\tlocation ~ "^/ngx_pagespeed_static/" { }\n\tlocation ~ "^/ngx_pagespeed_beacon$" { }\n\tlocation /ngx_pagespeed_statistics { allow 127.0.0.1; deny all; }\n\tlocation /ngx_pagespeed_message { allow 127.0.0.1; deny all; }@' /root/lnmp/vhost.sh
+cp /root/lnmp/vhost.sh /root/lnmp/vhost_ngx_pagespeed.sh
+sed -i 's@root $vhostdir;@root $vhostdir;\n\tpagespeed on;\n\tpagespeed FileCachePath /var/ngx_pagespeed_cache;\n\tlocation ~ "\\.pagespeed\\.([a-z]\\.)?[a-z]{2}\\.[^.]{10}\\.[^.]+" { add_header "" ""; }\n\tlocation ~ "^/ngx_pagespeed_static/" { }\n\tlocation ~ "^/ngx_pagespeed_beacon$" { }\n\tlocation /ngx_pagespeed_statistics { allow 127.0.0.1; deny all; }\n\tlocation /ngx_pagespeed_message { allow 127.0.0.1; deny all; }@' /root/lnmp/vhost_ngx_pagespeed.sh
+echo -e "add Virtual Hosts:               \033[32m/root/lnmp/vhost.sh\033[0m"
+echo -e "add ngx_pagespeed Virtual Hosts: \033[32m/root/lnmp/vhost_ngx_pagespeed.sh\033[0m"
+fi

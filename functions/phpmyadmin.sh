2cdbfcf097d7f63b0940911968605047799a7a0e
lj2007331 <lj2007331@163.com>
2013-09-25 20:08:09 +0800

    phpMyAdmin error

--- functions/phpmyadmin.sh
+++ functions/phpmyadmin.sh
@@ -8,7 +8,7 @@ cd $lnmp_dir/src
 . ../functions/download.sh 
 . ../options.conf 
 
-src_url=http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.0.7/phpMyAdmin-4.0.7-all-languages.zip && Download_src
+src_url=http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.0.7/phpMyAdmin-4.0.7-all-languages.tar.gz && Download_src
 
 tar xzf phpMyAdmin-4.0.7-all-languages.tar.gz
 /bin/mv phpMyAdmin-4.0.7-all-languages $home_dir/default/phpMyAdmin

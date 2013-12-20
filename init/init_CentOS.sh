b8ae5b84ebbe7c64ba39b99b5011fe10cd291c36
lj2007331 <lj2007331@163.com>
2013-12-20 14:38:21 +0800

    change yum

--- init/init_CentOS.sh
+++ init/init_CentOS.sh
@@ -16,24 +16,24 @@ public_IP=`../functions/get_public_ip.py`
 if [ "`../functions/get_ip_area.py $public_IP`" == 'CN' ];then
         mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
 	if [ ! -z "$(cat /etc/redhat-release | grep '6\.')" ];then
-		wget -c http://mirrors.163.com/.help/CentOS6-Base-163.repo -P /etc/yum.repos.d
-		#wget -c http://blog.linuxeye.com/wp-content/uploads/2013/12/CentOS6-Base.repo -P /etc/yum.repos.d
+		#wget -c http://mirrors.163.com/.help/CentOS6-Base-163.repo -P /etc/yum.repos.d
+		wget -c http://blog.linuxeye.com/wp-content/uploads/2013/12/CentOS6-Base.repo -P /etc/yum.repos.d
 		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS6-Base-163.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS6-Base-163.repo
-			#sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS6-Base.repo
-	                #sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS6-Base.repo
+			#sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS6-Base-163.repo
+	                #sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS6-Base-163.repo
+			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS6-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS6-Base.repo
 		fi
 	fi
 
 	if [ ! -z "$(cat /etc/redhat-release | grep '5\.')" ];then
-		wget -c http://mirrors.163.com/.help/CentOS5-Base-163.repo -P /etc/yum.repos.d
-		#wget -c http://blog.linuxeye.com/wp-content/uploads/2013/12/CentOS5-Base.repo -P /etc/yum.repos.d
+		#wget -c http://mirrors.163.com/.help/CentOS5-Base-163.repo -P /etc/yum.repos.d
+		wget -c http://blog.linuxeye.com/wp-content/uploads/2013/12/CentOS5-Base.repo -P /etc/yum.repos.d
 		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS5-Base-163.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS5-Base-163.repo
-			#sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS5-Base.repo
-	                #sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS5-Base.repo
+			#sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS5-Base-163.repo
+	                #sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS5-Base-163.repo
+			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS5-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS5-Base.repo
 		fi
 	fi
 

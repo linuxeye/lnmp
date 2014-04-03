dc8da9d2ba050077daeee3ab8dbc8d0ae5a45823
ywfwj2008 <ywfwj2008@users.noreply.github.com>
2014-04-03 12:49:15 +0800

    Update memcached.sh

--- functions/memcached.sh
+++ functions/memcached.sh
@@ -10,8 +10,8 @@ cd $lnmp_dir/src
 . ../options.conf
 
 src_url=http://blog.linuxeye.com/lnmp/src/memcached-1.4.17.tar.gz && Download_src
-src_url=https://launchpad.net/libmemcached/1.0/1.0.16/+download/libmemcached-1.0.16.tar.gz && Download_src
-src_url=http://pecl.php.net/get/memcached-2.1.0.tgz && Download_src
+src_url=https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz && Download_src
+src_url=http://pecl.php.net/get/memcached-2.2.0.tgz && Download_src
 src_url=http://pecl.php.net/get/memcache-2.2.7.tgz && Download_src
 
 # memcached server
@@ -53,14 +53,14 @@ else
 fi
 
 # php memcached extension
-tar xzf libmemcached-1.0.16.tar.gz
-cd libmemcached-1.0.16
+tar xzf libmemcached-1.0.18.tar.gz
+cd libmemcached-1.0.18
 ./configure --with-memcached=$memcached_install_dir
 make && make install
 cd ..
 
-tar xzf memcached-2.1.0.tgz
-cd memcached-2.1.0
+tar xzf memcached-2.2.0.tgz
+cd memcached-2.2.0
 make clean
 $php_install_dir/bin/phpize
 ./configure --with-php-config=$php_install_dir/bin/php-config

9abd3c01270ee25adaadcb8d133ea8cf731ce4f6
lj2007331 <lj2007331@163.com>
2014-05-02 23:01:53 +0800

    update php version

--- functions/tcmalloc.sh
+++ functions/tcmalloc.sh
@@ -7,10 +7,10 @@ Install_tcmalloc()
 cd $lnmp_dir/src
 . ../functions/download.sh
 
-src_url=http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz && Download_src
 src_url=http://gperftools.googlecode.com/files/gperftools-2.1.tar.gz && Download_src
 
 if [ `getconf WORD_BIT` == 32 ] && [ `getconf LONG_BIT` == 64 ];then
+	src_url=http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz && Download_src
 	tar xzf libunwind-1.1.tar.gz
 	cd libunwind-1.1
 	CFLAGS=-fPIC ./configure

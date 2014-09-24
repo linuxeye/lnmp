5249684d0bf8966c2dc9e9a3b17c4fb0c6a6d89e
lj2007331 <lj2007331@gmail.com>
2014-09-24 21:45:43 +0800

    update MySQL version

--- functions/mysql-5.6.sh
+++ functions/mysql-5.6.sh
@@ -10,7 +10,7 @@ cd $lnmp_dir/src
 . ../options.conf
 
 src_url=http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz && Download_src 
-src_url=http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.20.tar.gz && Download_src
+src_url=http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.21.tar.gz && Download_src
 
 useradd -M -s /sbin/nologin mysql
 mkdir -p $mysql_data_dir;chown mysql.mysql -R $mysql_data_dir
@@ -22,8 +22,8 @@ if [ ! -e "`which cmake`" ];then
         cd ..
 	/bin/rm -rf cmake-3.0.2
 fi
-tar zxf mysql-5.6.20.tar.gz
-cd mysql-5.6.20
+tar zxf mysql-5.6.21.tar.gz
+cd mysql-5.6.21
 if [ "$je_tc_malloc" == '1' ];then
         EXE_LINKER="-DCMAKE_EXE_LINKER_FLAGS='-ljemalloc'"
 elif [ "$je_tc_malloc" == '2' ];then
@@ -59,7 +59,7 @@ chkconfig mysqld on'
 OS_Debian_Ubuntu='update-rc.d mysqld defaults'
 OS_command
 cd ..
-/bin/rm -rf mysql-5.6.20
+/bin/rm -rf mysql-5.6.21
 cd ..
 
 # my.cf

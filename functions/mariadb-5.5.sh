3db82902a74647a3511535f972534e710d3c5ba9
lj2007331 <lj2007331@gmail.com>
2014-10-10 15:33:26 +0800

    update mariadb 5.5.39 to 5.5.40

--- functions/mariadb-5.5.sh
+++ functions/mariadb-5.5.sh
@@ -10,7 +10,7 @@ cd $lnmp_dir/src
 . ../options.conf
 
 src_url=http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz && Download_src 
-src_url=https://downloads.mariadb.org/f/mariadb-5.5.39/source/mariadb-5.5.39.tar.gz && Download_src 
+src_url=https://downloads.mariadb.org/f/mariadb-5.5.40/source/mariadb-5.5.40.tar.gz && Download_src 
 
 useradd -M -s /sbin/nologin mysql
 mkdir -p $mariadb_data_dir;chown mysql.mysql -R $mariadb_data_dir
@@ -22,8 +22,8 @@ if [ ! -e "`which cmake`" ];then
         cd ..
 	/bin/rm -rf cmake-3.0.2
 fi
-tar zxf mariadb-5.5.39.tar.gz
-cd mariadb-5.5.39
+tar zxf mariadb-5.5.40.tar.gz
+cd mariadb-5.5.40
 if [ "$je_tc_malloc" == '1' ];then
 	EXE_LINKER="-DCMAKE_EXE_LINKER_FLAGS='-ljemalloc'"
 elif [ "$je_tc_malloc" == '2' ];then
@@ -63,7 +63,7 @@ chkconfig mysqld on'
 OS_Debian_Ubuntu='update-rc.d mysqld defaults'
 OS_command
 cd ..
-/bin/rm -rf mariadb-5.5.39 
+/bin/rm -rf mariadb-5.5.40 
 cd ..
 
 # my.cf
--- functions/test.sh
+++ functions/test.sh
@@ -8,12 +8,22 @@ cd $lnmp_dir/src
 . ../functions/download.sh 
 . ../options.conf
 
-src_url=http://www.yahei.net/tz/tz.zip && Download_src
+public_IP=`../functions/get_public_ip.py`
+if [ "`../functions/get_ip_area.py $public_IP`" == 'CN' ];then
+	FLAG_IP=CN
+fi
+
+if [ "$FLAG_IP"x == "CN"x ];then
+	src_url=http://www.yahei.net/tz/tz.zip && Download_src
+	unzip -q tz.zip -d $home_dir/default
+else
+	src_url=http://www.yahei.net/tz/tz_e.zip && Download_src
+	unzip -q tz_e.zip -d $home_dir/default;/bin/mv $home_dir/default/{tz_e.php,tz.php}
+fi
 src_url=https://gist.githubusercontent.com/ck-on/4959032/raw/0b871b345fd6cfcd6d2be030c1f33d1ad6a475cb/ocp.php && Download_src
 
 echo '<?php phpinfo() ?>' > $home_dir/default/phpinfo.php
 /bin/cp $lnmp_dir/conf/index.html $home_dir/default
-unzip -q tz.zip -d $home_dir/default
 [ "$PHP_cache" == '1' ] && /bin/cp ocp.php $home_dir/default && sed -i 's@<a href="/xcache" target="_blank" class="links">xcache</a>@<a href="/ocp.php" target="_blank" class="links">Opcache</a>@' $home_dir/default/index.html
 [ "$PHP_cache" == '3' ] && sed -i 's@<a href="/xcache" target="_blank" class="links">xcache</a>@<a href="/apc.php" target="_blank" class="links">APC</a>@' $home_dir/default/index.html
 [ "$PHP_cache" == '4' ] && /bin/cp eaccelerator-*/control.php $home_dir/default && sed -i 's@<a href="/xcache" target="_blank" class="links">xcache</a>@<a href="/control.php" target="_blank" class="links">eAccelerator</a>@' $home_dir/default/index.html
@@ -23,4 +33,4 @@ chown -R www.www $home_dir/default
 [ -e "$db_install_dir" -a -z "`ps -ef | grep -v grep | grep mysql`" ] && service mysqld start
 [ -e "$apache_install_dir" -a -z "`ps -ef | grep -v grep | grep apache`" ] && service httpd restart 
 cd ..
-}
+

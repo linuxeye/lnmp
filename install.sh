0de4f100e9cba1123e9b1bcaadf732cd9ec7b857
lj2007331 <lj2007331@gmail.com>
2015-04-08 17:53:28 +0800

    added ZendGuardLoader php5.5/5.6

--- install.sh
+++ install.sh
@@ -294,7 +294,7 @@ else
                                                 (( ${#xcache_admin_pass} >= 5 )) && { xcache_admin_md5_pass=`echo -n "$xcache_admin_pass" | md5sum | awk '{print $1}'` ; break ; } || echo -e "\033[31mxcache admin password least 5 characters! \033[0m"
                                         done
                                 fi
-				if [ "$PHP_version" == '1' -o "$PHP_version" == '2' ];then
+				if [ "$PHP_version" != '5' ];then
                                         while :
                                         do
                                                 echo
